70d8a994a20ac3578d1b9123b4d98c6939404c16
Jayalls <jason.allais@outlook.com>
2023-11-19 09:32:55 +0100

    Burger Menu for tablet and mobile Added

--- src/app/component/Header/Header.js
+++ src/app/component/Header/Header.js
@@ -1,19 +1,33 @@
+'use client'
+import { useState } from 'react';
 import Link from 'next/link';
-import styles from "./style.module.scss"
+import styles from "./style.module.scss";
 
-const Header = () => (
-    <header className={styles.header}>
-        <div className={styles.logo}>CryptoLogo</div>
-        <nav className={styles.nav}>
-            <ul>
-                <li><Link className={styles.link} href="/explore">Explore</Link></li>
-                <li><Link className={styles.link} href="/collection">Collection</Link></li>
-                <li><Link  className={styles.link} href="/nfts">NFT's</Link></li>
-                <li><Link className={styles.link} href="/blog">Blog</Link></li>
-                <li><Link className={styles.link} href="/about">About</Link></li>
-            </ul>
-        </nav>
-    </header>
-);
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>CryptoLogo</div>
+
+            <button className={styles.burgerMenuButton} onClick={toggleMenu}>
+                <img src={isMenuOpen ? "./fermer.png" : "./menu.png"} alt="Menu" />
+            </button>
+
+            <nav className={`${styles.nav} ${isMenuOpen ? styles.navActive : ''}`}>
+                <ul>
+                    <li><Link className={styles.link} href="/explore">Explore</Link></li>
+                    <li><Link className={styles.link} href="/collection">Collection</Link></li>
+                    <li><Link  className={styles.link} href="/nfts">NFT's</Link></li>
+                    <li><Link className={styles.link} href="/blog">Blog</Link></li>
+                    <li><Link className={styles.link} href="/about">About</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
+
